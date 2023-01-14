efc5433b9a533547d254639f95ba550299616c89
VION79 <vion79@gmail.com>
2023-01-14 15:56:42 -0500

    28checkForAllOrNone

---
+++ 25specifyUpperLowerNumber.js
@@ -0,0 +1,48 @@
+/*
+Specify Upper and Lower Number of Matches
+Recall that you use the plus sign + to look for one or more characters and the asterisk * to look for zero or more characters. These are convenient but sometimes you want to match a certain range of patterns.
+
+You can specify the lower and upper number of patterns with quantity specifiers. Quantity specifiers are used with curly brackets ({ and }). You put two numbers between the curly brackets - for the lower and upper number of patterns.
+
+For example, to match only the letter a appearing between 3 and 5 times in the string ah, your regex would be /a{3,5}h/.
+
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+The first test call would return true, while the second would return false.
+
+Change the regex ohRegex to match the entire phrase Oh no only when it has 3 to 6 letter h's.
+
+Your regex should use curly brackets.
+Waiting:Your regex should not match the string Ohh no
+Waiting:Your regex should match the string Ohhh no
+Waiting:Your regex should match the string Ohhhh no
+Waiting:Your regex should match the string Ohhhhh no
+Waiting:Your regex should match the string Ohhhhhh no
+Waiting:Your regex should not match the string Ohhhhhhh no
+
+let ohStr = "Ohhh no";
+let ohRegex = /change/; // Change this line
+let result = ohRegex.test(ohStr);
+
+
+
+*/
+
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6}\sno/; // Change this line
+let result = ohRegex.test(ohStr);
+
+console.log(result);
+
+
+/*
+
+E:\githubrepo_local\Regex>node 25specifyUpperLowerNumber.js
+true
+
+E:\githubrepo_local\Regex>
+
+*/
